1aaff608779c24d5971ec79c6065bfa3d03d52cc
Jomar Gotoc <jopa.gotoc.up@phinmaed.com>
2023-08-17 04:13:24 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal ({
+  distance: '25px',
+  duration: 2500,
+  reset: true
+})
+
+sr.reveal('.home',{delay:190, origin:'bottom'})
+sr.reveal('.service_section',{delay:200, origin:'bottom'})
